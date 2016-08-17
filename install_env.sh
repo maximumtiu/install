ba7783b269b567fea41ceacb752ec9178862bd47
Megan Tiu <megantiu@me.com>
2016-08-17 00:23:09 -0400

    Initial commit

---
+++ install_env.sh
@@ -0,0 +1,28 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+\curl -sSL https://get.rvm.io | bash --ruby=2.3.1
+
+brew update
+brew tap caskroom/cask
+brew install bash
+brew install git
+brew install vim
+brew install coreutils
+brew install imagemagick
+brew install postgresql
+brew install redis
+brew install zsh
+brew cask install flux
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install slack
+brew cask install alfred
+brew cask install caffeine
+brew cask install spotify
+
+rvm install 2.3.1
+rvm use 2.3.1 --default
+gem install bundler
+gem install homesick
+
+homesick clone megantiu/dotfiles
+homesick symlink dotfiles
