a0d306941f1a485187aa52b9f04e416700a66210
Megan Tiu <megantiu@me.com>
2018-07-16 08:53:43 -0400

    Version 2.0, I guess
    Use a Brewfile to manage brew packages and apps, automate a little extra
    with a setup script

--- install_env.sh
+++ install_env.sh
@@ -1,34 +1,11 @@
+#!/bin/bash
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-\curl -sSL https://get.rvm.io | bash
-rvm install 2.4
-rvm use 2.4
+brew bundle
 
-brew update
-brew tap caskroom/cask
-brew install bash
-brew install git
-brew install vim
-brew install coreutils
-brew install imagemagick
-brew install postgresql
-brew install redis
-brew install tree
-brew install zsh
+curl -sSL https://get.rvm.io | bash -s stable --ruby=2.4 --gems=bundler,homesick
 
-brew cask install flux
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install slack
-brew cask install alfred
-brew cask install caffeine
-brew cask install spotify
-
-# callrail-specific
-brew cask install skype
-brew cask install 1password
-
-gem install bundler
-gem install homesick
-
+(
 homesick clone megantiu/dotfiles
 homesick symlink dotfiles
+)
