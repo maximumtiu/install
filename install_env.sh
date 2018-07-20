a76894c0c8239a7a7446e6f2dd192aa64352714d
Megan Tiu <megantiu@me.com>
2018-07-20 12:07:18 -0400

    Install vundle, vim plugins, and powerline font

--- install_env.sh
+++ install_env.sh
@@ -9,3 +9,8 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby=2.4 --gems=bundler,homesick
 homesick clone megantiu/dotfiles
 homesick symlink dotfiles
 )
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+[[ ! -d ~/Library/Fonts ]] && mkdir ~/Library/Fonts
+cp ./droid_sans_mono_dotted_powerline.ttf
