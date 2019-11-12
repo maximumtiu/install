4dc3b528712f594850200966814139f9d713c463
Megan Tiu <megantiu@me.com>
2019-11-12 12:05:49 -0500

    Fix miscellaneous bugs

--- install_env.sh
+++ install_env.sh
@@ -6,11 +6,18 @@ brew bundle
 curl -sSL https://get.rvm.io | bash -s stable --ruby=2.6 --gems=bundler,homesick
 
 (
+source /Users/megan/.rvm/scripts/rvm
 homesick clone megantiu/dotfiles
 homesick symlink dotfiles
-)
+cd ~/.homesick/repos/dotfiles
+bin/install
 
+# install and setup vim
+[[ ! -d ~/.vim/tmp ]] && mkdir ~/.vim/tmp
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# install preferred font
 [[ ! -d ~/Library/Fonts ]] && mkdir ~/Library/Fonts
-cp ./droid_sans_mono_dotted_powerline.ttf
+cp ./droid_sans_mono_dotted_powerline.ttf ~/Library/Fonts/droid_sans_mono_dotted_powerline.ttf
+)
